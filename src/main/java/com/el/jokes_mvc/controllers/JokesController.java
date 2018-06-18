e591c2c35fba40b0bcff5b7cf7e6556688364bd9
Jason Medcoff <jason.medcoff@ally.com>
2018-06-18 16:48:07 -0400

    finished crossing the omniverse

--- src/main/java/com/el/jokes_mvc/controllers/JokesController.java
+++ src/main/java/com/el/jokes_mvc/controllers/JokesController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.el.jokes_mvc.models.Joke;
@@ -41,6 +42,37 @@ public class JokesController {
 	public ModelAndView create(String joke, String punchline, int rating) {
 		ModelAndView mv = new ModelAndView();
 		jokeRepository.save(new Joke(joke, punchline, rating));
+		mv.setViewName("read");
+		return mv;
+	}
+	
+	@GetMapping("/edit")
+	public ModelAndView edit() {
+		ModelAndView mv = new ModelAndView();
+		List<Joke> jokes = jokeRepository.findAll();
+		mv.addObject("jokes", jokes);
+		mv.setViewName("edit");
+		return mv;
+	}
+	
+	@PostMapping("/edit")
+	public ModelAndView update(int id, String joke, String punchline, int rating) {
+		ModelAndView mv = new ModelAndView();
+		Joke jokeObj = jokeRepository.findOne(id);
+		jokeObj.setJoke(joke);
+		jokeObj.setPunchline(punchline);
+		jokeObj.setRating(rating);
+		//jokeRepository.delete(id);
+		jokeRepository.save(jokeObj);
+		
+		mv.setViewName("read");
+		return mv;
+	}
+	
+	@PostMapping("/delete")
+	public ModelAndView delete(int id) {
+		ModelAndView mv = new ModelAndView();
+		jokeRepository.delete(id);
 		mv.setViewName("read");
 		return mv;
 	}
