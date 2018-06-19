d6f20015cc8528b3b1a72c7130ab144e6759d6a0
Evan Bradley <evan.bradley@ally.com>
2018-06-19 09:25:48 -0400

    Cleaned up JokesController and added links between pages.

--- src/main/java/com/el/jokes_mvc/controllers/JokesController.java
+++ src/main/java/com/el/jokes_mvc/controllers/JokesController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.el.jokes_mvc.models.Joke;
